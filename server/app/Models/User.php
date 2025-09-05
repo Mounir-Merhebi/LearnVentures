860cc04601123f5e960a6464c7e282c68552bff0
MounirPrograms <mounirmerhebi21@gmail.com>
2025-09-05 16:22:57 +0300

    fixed the code in the user model

--- server/app/Models/User.php
+++ server/app/Models/User.php
@@ -3,8 +3,35 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class User extends Model
 {
-    //
+    use HasFactory;
+
+    // table is 'users' by default
+
+    // string/uuid PK
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+    // timestamps: only created_at
+    public $timestamps = false;
+    const CREATED_AT = 'created_at';
+    const UPDATED_AT = null;
+
+    protected $fillable = [
+        'id',
+        'email',
+        'password',
+        'role',
+        'name',
+        'hobbies',
+        'preferences',
+        'bio',
+        'excel_sheet_path',
+        'created_at',
+    ];
+
+    protected $hidden = ['password'];
 }
