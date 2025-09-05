598f87266c23612bb3acda19e0b3f488546f7712
MounirPrograms <mounirmerhebi21@gmail.com>
2025-09-05 13:02:56 +0300

    added the route for the auth

--- client/src/routes/route.jsx
+++ client/src/routes/route.jsx
@@ -1,12 +1,13 @@
-import{Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import LandingPage from "../pages/LandingPage";
-
+import Auth from "../pages/Auth";
 
 const MyRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<LandingPage/>} />
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/auth" element={<Auth />} />
     </Routes>
   );
 };
